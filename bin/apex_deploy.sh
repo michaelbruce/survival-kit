8b9c0516c074ea9d83169587f9231db1922aea07
Michael Bruce <michael.bruce@singletracksystems.com>
2015-10-27 11:39:39 +0000

    Included survival-kit bin folder in PATH

--- bin/apex_deploy.sh
+++ bin/apex_deploy.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # Args:
-#   1. filepath e.g src/pages/SendEmail.page
+#   1. target filepath relative to project e.g src/pages/SendEmail.page
 #   2. tooling-force jar filepath
 #   3. .properties config filepath
+#   4. project filepath
 
 echo -e "$1\n$1-meta.xml" > /tmp/currentFile
-java -jar $2
-        \ --action=deploySpecificFiles
-        \ --config=$3
-        \ --projectPath=$2
-        \ --responseFilePath=/tmp/responseFile
-        \ --specificFiles=/tmp/currentFile
-        \ --ignoreConflicts=true
-        \ --pollWaitMillis=1000'
+java -jar $2 --action=deploySpecificFiles \
+             --config=$3 \
+             --projectPath=$4 \
+             --responseFilePath=/tmp/responseFile \
+             --specificFiles=/tmp/currentFile \
+             --ignoreConflicts=true \
+             --pollWaitMillis=1000
